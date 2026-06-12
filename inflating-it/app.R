3567dbbe9a66ed22d0d9f6fd510c3d273b76c4e5
David Price <dreedprice@gmail.com>
2026-06-12 17:16:12 -0500

    Stand alone shiny dashboards.

---
+++ inflating-it/app.R
@@ -0,0 +1,20 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+library(shiny)
+library(bslib)
+library(tidyverse)
+#
+# Define UI for application
+ui <- fluidPage()
+
+# Define server logic required
+server <- function(input, output) {}
+
+# Run the application
+shinyApp(ui = ui, server = server)
